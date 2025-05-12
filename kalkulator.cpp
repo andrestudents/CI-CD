27220a4ea9a55d05e541c7806dbc51cea0512a33
AndStyNgh <andreassetyo82@gmail.com>
2025-05-12 20:46:13 +0700

    initial commit

---
+++ kalkulator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char op;
+
+    cout << "Masukkan angka pertama: ";
+    cin >> num1;
+
+    cout << "Pilih operator : \n";
+    cout << "	( + ) \n"; 
+    cout << "	( - ) \n"; 
+    cout << "	( x ) \n"; 
+    cout << "	( : ) \n";
+	cout << "Pilih : "; 
+    cin >> op;
+    
+
+    cout << "Masukkan angka kedua: ";
+    cin >> num2;
+
+    if (op == '+') {
+        cout << "Hasil: " << num1 + num2 << endl;
+    } 
+    else if (op == '-') {
+        cout << "Hasil: " << num1 - num2 << endl;
+    } 
+    else if (op == '*') {
+        cout << "Hasil: " << num1 * num2 << endl;
+    } 
+    else if (op == '/') {
+        if (num2 != 0) {
+            cout << "Hasil: " << num1 / num2 << endl;
+        } else {
+            cout << "Error! Pembagian dengan nol." << endl;
+        }
+    } 
+    else {
+        cout << "Operator tidak valid!" << endl;
+    }
+
+    return 0;
+}
+
